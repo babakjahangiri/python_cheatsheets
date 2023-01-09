f5e6b4f8dffc111b0b666cbe28273e0cde910da5
Babak Jahangiri <babakjahangiri@gmail.com>
2023-01-09 00:24:59 +0000

    update dictionary

--- DataStructure/Built-in/dictionary.py
+++ DataStructure/Built-in/dictionary.py
@@ -140,3 +140,19 @@ x = person.setdefault("id", "1000")
 print(x)
 
 print(person) #{'name': 'Tom', 'lastname': 'Ford', 'dob': 1988, 'married': True, 'balance': 1600.0, 'id': '1000'}
+
+
+#---- dictionary maker
+
+def names_dict(**kwargs):
+    return kwargs
+
+print(names_dict(Jane = 'Doe', John = 'Smith')) # {'Jane': 'Doe', 'John': 'Smith'}
+
+
+
+# ---- merging two dictionaries
+num_dict = {'a': 1, 'b': 2, 'c': 3}
+num_dict_2 = {'d': 4, 'e': 5, 'f': 6}
+new_dict = {**num_dict, **num_dict_2}
+print(new_dict) # {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
