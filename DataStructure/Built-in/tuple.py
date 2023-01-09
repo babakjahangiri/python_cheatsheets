fd5e9fa8145cf0bd4191e704d7d15c0e320bc85b
Babak Jahangiri <babakjahangiri@gmail.com>
2023-01-09 00:57:08 +0000

    update tuples

--- DataStructure/Built-in/tuple.py
+++ DataStructure/Built-in/tuple.py
@@ -103,3 +103,49 @@ def names_tuple(*args):
     return args
 
 print(names_tuple('Michael', 'John', 'Nancy')) # ('Michael', 'John', 'Nancy')
+
+
+# --- loop through a tuple
+for x in good_buddies:
+  print(x)
+
+# -- loop through the tuple items by referring to their index number
+myfriends = ('amir', 'sufi', 'sarah', 'alireza', 'fati', 'erfan', 'mahsa', 'bijan')
+print('---- loop through the tuple items ----')
+for i in range(5,len(myfriends)):
+  print(myfriends[i],end="\f")
+
+# -- while loop
+print('\n')
+print('---- Using a While Loop ----')
+i = 0
+while i < len(myfriends):
+  print(i+1,'-',myfriends[i])
+  i+=1
+
+
+print('\n')
+#---- join two or more tuples with + operator
+
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3) # ('a', 'b', 'c', 1, 2, 3)
+
+
+# --- multiply the content of a tuple
+fruits = ("blueberry", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple) #('blueberry', 'banana', 'cherry', 'blueberry', 'banana', 'cherry')
+
+num_banana = mytuple.count('banana')
+print(num_banana)
+
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 7)
+index_seven= thistuple.index(7)
+print(index_seven) #2   it returns the first occurrence 
+
+#thistuple.index(700)
+#ValueError: tuple.index(x): x not in tuple
